6994d5415d5911142717ff7a9647ace45cb7bc47
Francisco Nieto <francisjavinico@gmail.com>
2025-07-16 08:35:32 +0200

    Fix: ajustes para build en Railway

--- config.ts
+++ config.ts
@@ -1,21 +1,9 @@
 /* eslint-disable n/no-process-env */
 
-import path from "path";
 import dotenv from "dotenv";
+dotenv.config(); // Carga .env en local, y en Railway usa process.env
+
 import moduleAlias from "module-alias";
-
-// Check the env
-const NODE_ENV = process.env.NODE_ENV ?? "development";
-
-if (NODE_ENV === "development") {
-  // Solo en local, carga archivo .env
-  dotenv.config({
-    path: path.join(__dirname, "./config/.env.development"),
-  });
-} else {
-  // En producción (Railway), usa variables de entorno ya definidas
-  dotenv.config(); // carga desde process.env directamente
-}
 
 // Configure moduleAlias
 if (__filename.endsWith("js")) {
